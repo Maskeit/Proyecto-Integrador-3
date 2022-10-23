ed8f6f7d9a7a5ca20789a5c1165fc5300130bee4
Maskeit <miguelagustin182@gmail.com>
2022-10-23 15:59:29 -0500

    miguel cambios1.1

--- cliente.php
+++ cliente.php
@@ -1,9 +1,11 @@
 <?php session_start();
-
+/*
 if(isset($_SESSION['codigoCliente'])){
     require 'vistas/cliente.view.php';
 } else {
     header('Location: rscCliente/loginCliente.php');
     die();
 }
+*/
+require 'vistas/cliente.view.php';
 ?> 
