4462f8b7ecad088d2aef5f0f868e89eb88788736
Maskeit <miguelagustin182@gmail.com>
2022-10-26 20:19:56 -0500

    cambios en proceso

--- cliente.php
+++ cliente.php
@@ -1,5 +1,18 @@
 <?php session_start();
 
+/*
+include_once '../bd/conexion.php';
+$objeto = new Conexion();
+$conexion = $objeto->Conectar();
+
+$statement = $conexion->prepare('SELECT * FROM alta WHERE codigoCliente = :codigoCliente AND codigoCliente = :codigoCliente');
+$statement->execute(array(
+        ':codigoCliente' => $codigoCliente,
+       
+    ));
+*/
+
+
 if(isset($_SESSION['codigoCliente'])){
     require 'vistas/cliente.view.php';
 } else {
