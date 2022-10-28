e750bd5d3a51894e103639dec31dd86d57b147e7
Roberto-MR <bgutierrezJY@hotmail.com>
2022-10-27 23:41:58 -0500

    Update TablaAmort
    diseño de tabla amortización.
    Me di cuenta que el problema del print aparentemente es exclusivo de opera, si pueden pruebenlo en otros navegadores para ver si persiste.

--- amortizacion/formularioAmort.php
+++ amortizacion/formularioAmort.php
@@ -7,23 +7,26 @@ require '../vistas/header.php';
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>PruebaChistosona</title>
+	<title>Formulario de Amortización</title>
 </head>
 <body>
-<form action="tablaAmort.php" method="POST">
+	<form action="tablaAmort.php" method="POST">
 
-<h3>Nuestros prestamos cuentan con 5% de comisión anual</h3>
-<b>
-	¿Cuanto dinero desea?
-	<input type="numbers" name="dinero" placeholder="$$$$"><br>
-</b>
-<b>
-	¿En cuantos meses pagará?
-	<input type="numbers" name="meses" placeholder="Ej. 12 meses"><br>
-</b>
-<input type="submit" value="Subir">
+	<h2 class="fw-bolder text-center font-monospace text-white">Nuestros prestamos solo tienen un 5% de comisión anual!</h3>
 
-</form>
-
+	<div class="d-flex justify-content-center">
+		<div style="background-color: rgba(100, 150, 300, 0.2); " class="mb-4 p-3 border border-dark d-flex justify-content-center rounded-4" style="max-width: 95%;">
+			<b>
+			¿Cuanto dinero desea pedir?
+			<input class="mx-2"type="numbers" name="dinero" placeholder="$$$$"><br>
+			</b>
+			<b>
+			¿En cuantos meses pagará?
+			<input class="mx-2" type="numbers" name="meses" placeholder="Ej. 12 meses"><br>
+			</b>
+			<input class="mx-2" type="submit" value="Subir">
+		</div>
+	</div>
+	</form>
 </body>
 </html>
