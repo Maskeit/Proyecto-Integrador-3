40b2a65deeb69cfe4146def4adbed997fa1e5c96
AngelPerez10 <111476102+AngelPerez10@users.noreply.github.com>
2022-11-24 17:27:01 -0600

    Add files via upload

---
+++ historial.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="#" />  
+    <title>Tabla de Clientes</title>
+      
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <!-- CSS personalizado --> 
+    <link rel="stylesheet" href="css/mainCrud.css">  
+      
+      
+    <!--datables CSS básico-->
+    <link rel="stylesheet" type="text/css" href="assets/datatables/datatables.min.css"/>
+    <!--datables estilo bootstrap 4 CSS-->  
+    <link rel="stylesheet"  type="text/css" href="assets/datatables/DataTables-1.10.18/css/dataTables.bootstrap4.min.css">    
+    
+    <!--css de la tabla-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="./Style.css" />
+    <link rel="stylesheet" href="./mobile-style.css">
+  
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">  
+
+
+    
+</head>
+<body> 
+    <header>
+     
+    <!--comienza header-->
+    <header>
+    <div class="container-fluid p-0">
+      <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand" href="index.php">
+          <i class="bi bi-cash-coin fa-2x mx-3"></i>Unibank
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+          aria-label="Toggle navigation">
+          <i class="fas fa-align-right text-light"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <div class="mr-auto"></div>
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="./index.php">HOME
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item" >
+             <a class="nav-link" href="./funciones/cerrarSesion.php">
+                <?php if(isset($_SESSION['adminUser'])){
+                    echo "CERRAR SESION"; //se va mostrar cerrar sesion si es que el ejecutivo tiene una sesion iniciada
+                    } elseif (isset($_SESSION['usuarioEjecutivo'])) {
+                    echo "CERRAR SESION";
+                    } elseif (isset($_SESSION['codigoCliente'])) {
+                    echo "CERRAR SESION";
+                    }
+                    else {
+                         echo ""; //sino no muestra nada  
+                    }
+                  ?>
+              </a>
+              </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </header>
+    </header>    
+    <br>
+
+    <div class="container caja">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="table-responsive">        
+                    <table id="tablaUsuarios" class="table table-striped table-bordered table-condensed" style="width:100%" >
+                        <thead class="text-center">
+                            <tr>
+                                <th>ID</th>
+                                <th>Origen</th>
+                                <th>Destino</th>
+                                <th>Beneficiario</th>
+                                <th>Concepto</th>                                
+                                <th>Monto</th>  
+                                <th>Banco</th>
+                                <th>Fecha</th>
+                            </tr>
+                        </thead>
+                        <tbody>                           
+                        </tbody>        
+                    </table>               
+                </div>
+            </div>
+        </div>  
+    </div>   
+      
+    <!-- jQuery, Popper.js, Bootstrap JS -->
+    <script src="assets/jquery/jquery-3.3.1.min.js"></script>
+    <script src="assets/popper/popper.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+      
+    <!-- datatables JS -->
+    <script type="text/javascript" src="assets/datatables/datatables.min.js"></script>    
+     
+    <script type="text/javascript" src="js/historial.js"></script>  
+    
+    
+</body>
+</html>
